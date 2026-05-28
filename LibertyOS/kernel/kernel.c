a9d6702825e80cf5a880d02439ab5476dfdb9998
C7zyC0w <kaiseaman0@gmail.com>
2026-05-28 23:19:06 +0100

    New Terminal Features!
    Added Cursor blinking, backspace/delete-left, and full ascii support and shift detection!!!

--- LibertyOS/kernel/kernel.c
+++ LibertyOS/kernel/kernel.c
@@ -5,6 +5,8 @@ volatile uint16_t* vga = (volatile uint16_t*)0xB8000;
 
 int row = 0;
 int col = 0;
+int input_col = 0;
+int shift = 0;
 
 const char scancode_table[128] = {
     [0x01]=0,     // Escape.
@@ -16,15 +18,36 @@ const char scancode_table[128] = {
     [0x10]='q', [0x11]='w', [0x12]='e', [0x13]='r',
     [0x14]='t', [0x15]='y', [0x16]='u', [0x17]='i',
     [0x18]='o', [0x19]='p', [0x1A]='[', [0x1B]=']',
-    [0x1C]='\n',  // Enter
+    [0x1C]='\n',  // Enter / Return.
     [0x1E]='a', [0x1F]='s', [0x20]='d', [0x21]='f',
     [0x22]='g', [0x23]='h', [0x24]='j', [0x25]='k',
     [0x26]='l', [0x27]=';', [0x28]='\'', [0x29]='`',
     [0x2B]='\\',
     [0x2C]='z', [0x2D]='x', [0x2E]='c', [0x2F]='v',
     [0x30]='b', [0x31]='n', [0x32]='m', [0x33]=',',
-    [0x34]='.', [0x35]='/', [0x39]=' '
+    [0x34]='.', [0x35]='/', [0x39]=' ' // Space.
 };
+
+const char scancode_table_shift[128] = {
+    [0x02]='!', [0x03]='@', [0x04]='#', [0x05]='$',
+    [0x06]='%', [0x07]='^', [0x08]='&', [0x09]='*',
+    [0x0A]='(', [0x0B]=')', [0x0C]='_', [0x0D]='+',
+    [0x10]='Q', [0x11]='W', [0x12]='E', [0x13]='R',
+    [0x14]='T', [0x15]='Y', [0x16]='U', [0x17]='I',
+    [0x18]='O', [0x19]='P', [0x1A]='{', [0x1B]='}',
+    [0x1C]='\n', //Enter / Return.
+    [0x1E]='A', [0x1F]='S', [0x20]='D', [0x21]='F',
+    [0x22]='G', [0x23]='H', [0x24]='J', [0x25]='K',
+    [0x26]='L', [0x27]=':', [0x28]='"', [0x29]='~',
+    [0x2B]='|',
+    [0x2C]='Z', [0x2D]='X', [0x2E]='C', [0x2F]='V',
+    [0x30]='B', [0x31]='N', [0x32]='M', [0x33]='<',
+    [0x34]='>', [0x35]='?', [0x39]=' '
+};
+
+char input_buf[256];
+int buf_len = 0;
+int buf_cursor = 0;
 
 static inline void outb(uint16_t port, uint8_t value) {  
     __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
@@ -38,6 +61,7 @@ void hide_hw_cursor() {
 
 
 char scancode_to_char(uint8_t scancode) {
+    if (shift) return scancode_table_shift[scancode];
     return scancode_table[scancode];
 }
 
@@ -49,9 +73,11 @@ static inline uint8_t inb(uint16_t port) {
 }
 
 uint8_t user_input() {
-    while ((inb(0x64) & 1 )!= 1) {}
-    uint8_t input = inb(0x60);
-    return input;
+    if ((inb(0x64) & 1 )== 1) {
+        uint8_t input = inb(0x60);
+        return input;
+    } else {return 0;}
+    
 }
 
 void print_str(const char* msg) {
@@ -89,7 +115,8 @@ void erase_cursor() {
 }
 
 void kernel_main() {
-
+    int j = 0;
+    int cursor_status = 0;
 
     for (int i = 0; i < 80 * 25; i++) vga[i] = 0x0F20;
     
@@ -99,6 +126,7 @@ void kernel_main() {
     
     print_str(msg);
     print_str("\n\n\n>>> ");
+    input_col = col;
     draw_cursor();
     
     
@@ -106,13 +134,45 @@ void kernel_main() {
         uint8_t key = user_input();
         char c = scancode_to_char(key);
         
+        j++;
+        if (j >= 4000000) {
+            if (cursor_status == 0) {
+                cursor_status = 1;
+                draw_cursor();
+            } else {
+                cursor_status = 0;
+                erase_cursor();
+            }
+            j = 0;
+        }     
+        
+        if (key == 0x0E && col > input_col) {
+            erase_cursor();
+            col--;
+            vga[(row * 80) + col] = (uint16_t)((0x0F << 8) | ' ');
+            draw_cursor();
+            continue;
+        } 
+        if (key == 0x0E && col == input_col){
+            // Add bell.
+            continue;
+        }
+        
+        if (key == 0x2A || key == 0x36) shift = 1;
+        if (key == 0xAA || key == 0xB6) shift = 0;
+        
+        
         if (key >= 0x80 || c == 0) continue;
         
         erase_cursor();
         print_char(c);
         
-        if (c == '\n') print_str(">>> ");
+        if (c == '\n') {
+        print_str(">>> ");
+        input_col = col;
+        }
       
-        draw_cursor();
+        cursor_status =1; draw_cursor();
+        
     }
 }
